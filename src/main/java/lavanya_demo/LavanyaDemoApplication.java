9ad21bd822c0e7a01b0ccdf5e05b32ea197a8bae
Avinash <avinashsm05@gmail.com>
2024-12-27 14:37:44 +0530

    lavanya

---
+++ src/main/java/lavanya_demo/LavanyaDemoApplication.java
@@ -0,0 +1,13 @@
+package lavanya_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanyaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavanyaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lavanya_demo/LavanyaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lavanya_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanyaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
